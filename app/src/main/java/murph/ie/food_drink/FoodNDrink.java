46e7c53b5d16b7e5ebb35a1177fbae97b4b98522
Mur3ph <paulmurphymainstreet@gmail.com>
2016-08-10 21:54:36 +0100

    Navigation back n forth from activities and documentation

--- app/src/main/java/murph/ie/food_drink/FoodNDrink.java
+++ app/src/main/java/murph/ie/food_drink/FoodNDrink.java
@@ -1,31 +1,43 @@
 package murph.ie.food_drink;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
+import murph.ie.keepitlocal.MainActivity;
 import murph.ie.keepitlocal.R;
 
-public class FoodNDrink extends AppCompatActivity {
+public class FoodNDrink extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_ndrink);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Button m_btnFoodNDrink = (Button) findViewById(R.id.btnPreviousFoodNDrink);
+        m_btnFoodNDrink.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        switch (v.getId()) {
+            case R.id.btnPreviousFoodNDrink:
+                // do your code
+                Log.i("clicks", "You Clicked previous");
+                i = new Intent(FoodNDrink.this, MainActivity.class);
+                startActivity(i);
+                break;
+
+            default:
+                break;
+        }
     }
 
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,17 @@
 # Keep It Local - Android application
+
+###### Functionality Implemented:
+<ol>
+	<li>Home page with three buttons </li>
+	<li>Second page with three buttons </li>
+	<li>Second page accessible by Home page </li>
+	<li>Activity access and navigation</li>
+	
+</ol>
+
+###### Technologies Used:
+ <ol>
+	<li>Java</li>
+	<li>Android Studio - Intellij </li>
+	<li>XML</li>
+</ol>
