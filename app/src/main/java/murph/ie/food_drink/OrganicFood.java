9de59a16261d4695c8c4d5a94442edef48894fe2
Mur3ph <paulmurphymainstreet@gmail.com>
2016-09-08 23:52:27 +0100

    Finally creating a client age, finally

--- app/src/main/java/murph/ie/food_drink/OrganicFood.java
+++ app/src/main/java/murph/ie/food_drink/OrganicFood.java
@@ -1,15 +1,18 @@
 package murph.ie.food_drink;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import murph.ie.keepitlocal.R;
 
-public class OrganicFood extends AppCompatActivity {
+public class OrganicFood extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +21,24 @@ public class OrganicFood extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //Previous button to go back to Main activity page
+        Button m_btnPreviousOrganicFood = (Button) findViewById(R.id.btnPreviousOrganicFood);
+        m_btnPreviousOrganicFood.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        switch (v.getId()) {
+            case R.id.btnPreviousOrganicFood:
+                // do your code
+                Log.i("clicks", "You Clicked previous");
+                i = new Intent(OrganicFood.this, FruitNVeg.class);
+                startActivity(i);
+                break;
+            default:
+                break;
+        }
     }
 
 }
