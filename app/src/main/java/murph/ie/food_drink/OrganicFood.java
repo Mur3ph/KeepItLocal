b003d4a33cd86ddc6890a06ca9fab1e3f9df1881
Mur3ph <paulmurphymainstreet@gmail.com>
2016-10-13 22:10:31 +0100

    added a clickable list of items to organic products

--- app/src/main/java/murph/ie/food_drink/OrganicFood.java
+++ app/src/main/java/murph/ie/food_drink/OrganicFood.java
@@ -1,5 +1,6 @@
 package murph.ie.food_drink;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -7,12 +8,33 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import murph.ie.keepitlocal.MainActivity;
 import murph.ie.keepitlocal.R;
 
 public class OrganicFood extends AppCompatActivity implements View.OnClickListener{
+
+    private List<String> data = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,23 +44,105 @@ public class OrganicFood extends AppCompatActivity implements View.OnClickListen
         setSupportActionBar(toolbar);
 
         //Previous button to go back to Main activity page
-        Button m_btnPreviousOrganicFood = (Button) findViewById(R.id.btnPreviousOrganicFood);
-        m_btnPreviousOrganicFood.setOnClickListener(this);
+//        Button m_btnPreviousOrganicFood = (Button) findViewById(R.id.btnPreviousOrganicFood);
+//        m_btnPreviousOrganicFood.setOnClickListener(this);
+
+        ListView lv = (ListView) findViewById(R.id.listview);
+        generateListContent();
+        lv.setAdapter(new MyListAdaper(this, R.layout.list_of_organic_food, data));
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(OrganicFood.this, "List item was clicked at " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
-        Intent i;
-        switch (v.getId()) {
-            case R.id.btnPreviousOrganicFood:
-                // do your code
-                Log.i("clicks", "You Clicked previous");
-                i = new Intent(OrganicFood.this, FruitNVeg.class);
-                startActivity(i);
-                break;
-            default:
-                break;
+//        Intent i;
+//        switch (v.getId()) {
+//            case R.id.btnPreviousOrganicFood:
+//                // do your code
+//                Log.i("clicks", "You Clicked previous");
+//                i = new Intent(OrganicFood.this, FruitNVeg.class);
+//                startActivity(i);
+//                break;
+//            default:
+//                break;
+//        }
+    }
+
+    //################################################################################################
+    //################################################################################################
+    //################################################################################################
+
+    private void generateListContent() {
+        for(int i = 0; i < 55; i++) {
+            data.add("This is row number " + i);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class MyListAdaper extends ArrayAdapter<String> {
+        private int layout;
+        private List<String> mObjects;
+        private MyListAdaper(Context context, int resource, List<String> objects) {
+            super(context, resource, objects);
+            mObjects = objects;
+            layout = resource;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            ViewHolder mainViewholder = null;
+            if(convertView == null) {
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(layout, parent, false);
+                ViewHolder viewHolder = new ViewHolder();
+                viewHolder.thumbnail = (ImageView) convertView.findViewById(R.id.list_item_thumbnail);
+                viewHolder.title = (TextView) convertView.findViewById(R.id.list_item_text);
+                viewHolder.button = (Button) convertView.findViewById(R.id.list_item_btn);
+                convertView.setTag(viewHolder);
+            }
+            mainViewholder = (ViewHolder) convertView.getTag();
+            mainViewholder.button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getContext(), "Button was clicked for list item " + position, Toast.LENGTH_SHORT).show();
+                }
+            });
+            mainViewholder.title.setText(getItem(position));
+
+            return convertView;
+        }
+    }
+    public class ViewHolder {
+
+        ImageView thumbnail;
+        TextView title;
+        Button button;
     }
 
 }
