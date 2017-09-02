791f7f7bb6d96b35b948393e507f5eb07ce6e655
Mur3ph <paulmurphymainstreet@gmail.com>
2017-09-02 21:23:41 +0100

    create new tables and tidy sql

--- app/src/main/res/sql/create-table-statement.sql
+++ app/src/main/res/sql/create-table-statement.sql
@@ -1,9 +1,8 @@
 /* ORIGINAL DATABASE ALL WHOLE LOT EXECUTES TOGETHER......................................*/
 -- TODO Create check constraints on tables, columns
 
-
-CREATE TABLE CUSTOMER(
-    customer_id 	       NUMBER(11) NOT NULL,
+CREATE TABLE Users(
+    user_id 	           NUMBER(11) NOT NULL,
     first_name 		       VARCHAR(50) NOT NULL,
     last_name 		       VARCHAR(50) NOT NULL,
 	phone 		  	       VARCHAR(20) NOT NULL,
@@ -11,24 +10,41 @@ CREATE TABLE CUSTOMER(
 	card_number 	       VARCHAR(20) NOT NULL,
 	card_id_number 	       VARCHAR(20) NOT NULL,
 	valid_date 		       VARCHAR(20) NOT NULL,
-	CONSTRAINT pk_customer PRIMARY KEY (customer_id)
+	CONSTRAINT pk_user     PRIMARY KEY (user_id)
 );
 
-CREATE TABLE SELLER (
+
+CREATE TABLE Buyer(
+  buyer_id                          INTEGER        NOT NULL,
+  user_id 	                        NUMBER(11)     NOT NULL,
+  first_name 		                VARCHAR(50)    NOT NULL,
+  last_name 		                VARCHAR(50)    NOT NULL,
+  email 	                        VARCHAR(50)    NOT NULL,
+  username 		                    VARCHAR(50)    NOT NULL,
+  password 		                    VARCHAR(50)    NOT NULL,
+  min_budget                        NUMBER(10, 2)  NOT NULL,
+  max_budget                        NUMBER(10, 2)  NOT NULL,
+  CONSTRAINT pk_buyer               PRIMARY KEY (buyer_id),
+  CONSTRAINT fk_buyer_user          FOREIGN KEY (user_id) REFERENCES Users (user_id) ON DELETE CASCADE,
+  CONSTRAINT ck_max_min_budget      CHECK ( min_budget <= max_budget )
+);
+
+
+CREATE TABLE Seller (
     seller_id 		  	            NUMBER(11) NOT NULL,
-    customer_id 	                NUMBER(11) NOT NULL,
+    user_id 	                    NUMBER(11) NOT NULL,
     first_name 		                VARCHAR(50) NOT NULL,
     last_name 		                VARCHAR(50) NOT NULL,
     email 	                        VARCHAR(50) NOT NULL,
 	username 		                VARCHAR(50) NOT NULL,
 	password 		                VARCHAR(50) NOT NULL,
-	CONSTRAINT pk_user              PRIMARY KEY (seller_id),
-	CONSTRAINT fk_seller_customer   FOREIGN KEY (customer_id) REFERENCES CUSTOMER (customer_id) ON DELETE CASCADE
+	CONSTRAINT pk_seller            PRIMARY KEY (seller_id),
+	CONSTRAINT fk_seller_user       FOREIGN KEY (user_id) REFERENCES Users (user_id) ON DELETE CASCADE
 );
 
-CREATE TABLE ADDRESS(
+CREATE TABLE Address(
      address_id 	                NUMBER(11)  NOT NULL,
-     customer_id 	                NUMBER(11) NOT NULL,
+     user_id 	                    NUMBER(11) NOT NULL,
      address_line1 	                VARCHAR(50) NOT NULL,
      address_line2 	                VARCHAR(50) NOT NULL,
      city 		  	                VARCHAR(20) NOT NULL,
@@ -36,44 +52,56 @@ CREATE TABLE ADDRESS(
      country 		                VARCHAR(20) NOT NULL,
      zip 		  	                VARCHAR(20) NOT NULL,
      CONSTRAINT pk_address          PRIMARY KEY (address_id),
-     CONSTRAINT fk_address_customer FOREIGN KEY (customer_id) REFERENCES CUSTOMER (customer_id) ON DELETE CASCADE
+     CONSTRAINT fk_address_user     FOREIGN KEY (user_id) REFERENCES Users (user_id) ON DELETE CASCADE
 );
 
-							 
-CREATE TABLE LOCAL_ORDER (
+
+CREATE TABLE Local_Order (
     order_id 					    NUMBER(11) NOT NULL,
-    customer_id 			        NUMBER(11) NOT NULL,
+    user_id 			            NUMBER(11) NOT NULL,
     order_date  				    DATE,
     CONSTRAINT pk_order             PRIMARY KEY (order_id),
-    CONSTRAINT fk_order_customer    FOREIGN KEY (customer_id) REFERENCES CUSTOMER (customer_id) ON DELETE CASCADE
-);	
+    CONSTRAINT fk_order_user        FOREIGN KEY (user_id) REFERENCES Users (user_id) ON DELETE CASCADE
+);
 
 
-CREATE TABLE SUPPLIER(
-    supplier_id 		     NUMBER(11) NOT NULL,
+CREATE TABLE Supplier(
+    supplier_id 		     NUMBER(11)  NOT NULL,
     supplier_name			 VARCHAR(40) NOT NULL,
 	supplier_address 		 VARCHAR(30) NOT NULL,
 	supplier_phone 		     VARCHAR(20) NOT NULL,
 	CONSTRAINT pk_supplier   PRIMARY KEY (supplier_id)
 );
 
-						 
-CREATE TABLE PRODUCT(
-    product_id 				      NUMBER(11) NOT NULL,
-	supplier_id 				  NUMBER(11) NOT NULL,
-	product_code				  VARCHAR(30)NOT NULL,
-    product_name			      VARCHAR(30)NOT NULL,
+
+CREATE TABLE Product(
+    product_id 				      NUMBER(11)  NOT NULL,
+	supplier_id 				  NUMBER(11)  NOT NULL,
+	product_code				  VARCHAR(30) NOT NULL,
+    product_name			      VARCHAR(30) NOT NULL,
     product_price 			      NUMBER(7,2) NOT NULL,
     CONSTRAINT pk_product         PRIMARY KEY (product_id),
-	CONSTRAINT fk_prod_supplier   FOREIGN KEY (supplier_id) REFERENCES SUPPLIER (supplier_id) ON DELETE CASCADE
+	CONSTRAINT fk_prod_supplier   FOREIGN KEY (supplier_id) REFERENCES Supplier (supplier_id) ON DELETE CASCADE
 );
 
 
-CREATE TABLE ORDER_ITEM(
+CREATE TABLE Image
+(
+  image_id                  INTEGER         NOT NULL,
+  product_id                NUMBER(11)      NOT NULL,
+  image_name                VARCHAR2(30),
+  mime_type                 VARCHAR2(30),
+  image_content             BLOB,
+  CONSTRAINT pk_image       PRIMARY KEY (image_id),
+  CONSTRAINT fk_image_room  FOREIGN KEY (product_id) REFERENCES product (product_id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE Order_Item(
     order_id 		              NUMBER(11) NOT NULL,
     product_id 		              NUMBER(11) NOT NULL,
     CONSTRAINT pk_order_item      PRIMARY KEY (order_id, product_id),
-    CONSTRAINT fk_item_order      FOREIGN KEY (order_id)   REFERENCES LOCAL_ORDER (order_id)  ON DELETE CASCADE,
-	CONSTRAINT fk_item_product    FOREIGN KEY (product_id) REFERENCES PRODUCT (product_id)    ON DELETE CASCADE
+    CONSTRAINT fk_item_order      FOREIGN KEY (order_id)   REFERENCES Local_Order (order_id)  ON DELETE CASCADE,
+	CONSTRAINT fk_item_product    FOREIGN KEY (product_id) REFERENCES Product (product_id)    ON DELETE CASCADE
 );
 
